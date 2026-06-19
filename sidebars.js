85e80a48a4fd0a54b170a3ee51d8bbba4cb1cf4a
VupplaDedeepya <vuppaladedeepya@gmail.com>
2026-06-19 19:45:23 +0530

    Setting changes

--- sidebars.js
+++ sidebars.js
@@ -22,6 +22,16 @@ const sidebars = {
       items: [
         {
           type: 'category',
+          label: 'Organization Settings',
+          items: [
+            'Organization/Profile',
+            'Organization/Localization',
+            'Organization/WorkSchedule',
+            'Organization/Branding',
+          ],
+        },
+        {
+          type: 'category',
           label: 'User Management',
           items: [
             'user-management/overview',
@@ -37,6 +47,59 @@ const sidebars = {
             'Configure/Custom-Fields',
             'Configure/Leads',
             'Configure/Deals',
+            'Configure/Tags',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Integrations',
+          items: [
+            'Integrations/Lead-Capture',
+          ],
+        },
+         {
+          type: 'category',
+          label: 'Pulse Settings',
+          items: [
+            'Pulse-Settings/Overview',
+            {
+              type: 'category',
+              label: 'Channels',
+              items: [
+                'Pulse-Settings/Channels/Channels',
+                'Pulse-Settings/Channels/WebChat',
+                'Pulse-Settings/Channels/Widget-Setup',
+                'Pulse-Settings/Channels/Conversation-Flow',
+              ],
+            },
+            'Pulse-Settings/Users&Access',
+            'Pulse-Settings/AssignmentRules',
+            'Pulse-Settings/KnowledgeBase',
+            'Pulse-Settings/SupportAvailability',
+            'Pulse-Settings/GlobalSettings',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Automation',
+          items: [
+            {
+              type: 'category',
+              label: 'Workflows',
+              items: [
+                'Automation/Workflows/Overview',
+                'Automation/Workflows/Lead-Workflows',
+                'Automation/Workflows/Deal-Workflows',
+                'Automation/Workflows/Logic-Control-Nodes',
+                'Automation/Workflows/Workflow-Action-Nodes',
+                'Automation/Workflows/Timing-Delay-Nodes',
+                'Automation/Workflows/Communication-Actions',
+                'Automation/Workflows/Task-Activity-Actions',
+                'Automation/Workflows/Integration-Actions',
+              ],
+            },
+            'Automation/Sequences',
+            'Automation/Scoring',
           ],
         },
       ],
